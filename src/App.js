1ba24753ea4064142333dc30892f4f17be366738
eruzanov <evgeniy.ruzanov@piano.io>
2018-01-05 20:14:44 +0300

    add random

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import get from 'lodash/get';
 import './App.css';
 
 class App extends Component {
-  state = {
-    map: [
-      [1, 2, 3, 4],
-      [5, 6, 7, 8],
-      [9, 10, 11, 12],
-      [13, 14, 15, false]
-    ]
-  };
+  constructor(props) {
+    super(props);
+    let values = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,false];
+    const shuffleValues = [];
+
+    while (values.length) {
+      let index = Math.ceil(Math.random() * 10);
+      if (index >= values.length) {
+        index = 0;
+      }
+      shuffleValues.push(values[index]);
+      values = values.filter((val, i) => i !== index);
+    }
+    const map = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+    let index = 0;
+    this.state = {
+      map : map.map(row => row.map(() => shuffleValues[index++]))
+    }
+  }
 
   findEmpty(i, j) {
     switch (false) {
@@ -42,8 +58,8 @@ class App extends Component {
       <div className="container-puzzle">
         {this.state.map.map((row, i) => {
           return row.map((el, j) => el ?
-            <div key={el} className="puzzle" onClick={() => this.onClick(i, j)}>{el}</div> :
-            <div key={el} className="puzzle-empty" onClick={() => this.onClick(i, j)}/>
+            <div key={row.length * i + j} className="puzzle" onClick={() => this.onClick(i, j)}>{el}</div> :
+            <div key={row.length * i + j} className="puzzle-empty" onClick={() => this.onClick(i, j)}/>
           )
         })}
       </div>
